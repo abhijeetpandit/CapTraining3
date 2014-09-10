366eb3b030374ff5b9c372a8b0927e9b85c7f297
abpandit <abpandit@DIN56001205.corp.capgemini.com>
2014-09-10 14:28:48 +0530

    Removed bug, <context:annotationConfig was not used>

--- 005-SpringCollectionInjection/src/org/abhi/s1/dao/EmployeeDaoImpl.java
+++ 005-SpringCollectionInjection/src/org/abhi/s1/dao/EmployeeDaoImpl.java
@@ -1,29 +1,17 @@
 package org.abhi.s1.dao;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
+
+import javax.annotation.Resource;
 
 import org.abhi.s1.datamodel.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 
 public class EmployeeDaoImpl implements EmployeeDao {
 	
+	@Resource
 	private List<Employee> employees;
 	
-	public List<Employee> getEmployees() {
-		return employees;
-	}
-
-	public void setEmployees(List<Employee> employees) {
-		this.employees = employees;
-	}
-
 	@Override
 	public Employee getSingleEmployee(String kinId) {
 		for(Employee employee : employees) {
--- 005-SpringCollectionInjection/src/org/abhi/s1/test/TestListInjection.java
+++ 005-SpringCollectionInjection/src/org/abhi/s1/test/TestListInjection.java
@@ -1,14 +1,21 @@
 package org.abhi.s1.test;
 
+import org.abhi.s1.datamodel.Employee;
 import org.abhi.s1.service.EmployeeService;
 import org.junit.Test;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 
 public class TestListInjection {
 	@Test
 	public void testMapInjection() {
-		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("beans-config.xml");
+		GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
+		ctx.load("beans-config.xml");
+		ctx.refresh();
+		
 		EmployeeService employeeService = ctx.getBean(EmployeeService.class);
-		System.out.println(employeeService.getAllEmployees());
+		for(Employee employee : employeeService.getAllEmployees()) {
+			System.out.println(employee);
+		}
 	}
 }
