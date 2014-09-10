358984b8cd8e9cc94d1b5c9f3b30d125fa90485c
abpandit <abpandit@DIN56001205.corp.capgemini.com>
2014-09-10 10:34:32 +0530

    Added name to bean

--- 004-SpringDaoServiceWithAnnotationConfig/src/org/abhi/s1/service/EmployeeServiceImpl.java
+++ 004-SpringDaoServiceWithAnnotationConfig/src/org/abhi/s1/service/EmployeeServiceImpl.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Required;
 import org.springframework.stereotype.Service;
 
-@Service
+@Service("abc")
 public class EmployeeServiceImpl implements EmployeeService {
 
 	@Autowired
