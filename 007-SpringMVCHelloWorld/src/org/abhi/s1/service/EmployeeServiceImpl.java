2c718d88a3d5a81a45a9e7441bfa1d9604d53dff
abpandit <abpandit@DIN56001205.corp.capgemini.com>
2014-09-11 15:53:52 +0530

    Final changes done for employee search by kinid and name

---
+++ 007-SpringMVCHelloWorld/src/org/abhi/s1/service/EmployeeServiceImpl.java
@@ -0,0 +1,26 @@
+package org.abhi.s1.service;
+
+import java.util.List;
+
+import org.abhi.s1.dao.EmployeeDao;
+import org.abhi.s1.datamodel.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Required;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+	@Autowired
+	private EmployeeDao employeeDao;
+	
+	@Override
+	public Employee getEmployeeById(String kinId) {
+		return employeeDao.getSingleEmployee(kinId);
+	}
+
+	@Override
+	public List<Employee> getEmployeeByName(String name) {
+		return employeeDao.getEmployeesByName(name);
+	}
+}
