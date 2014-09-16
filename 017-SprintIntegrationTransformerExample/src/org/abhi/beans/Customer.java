0270f5e6285864db8ad5f8c056711104742bb8ee
abpandit <abpandit@DIN56001205.corp.capgemini.com>
2014-09-16 11:04:47 +0530

    Added project for Spring integration transformer example

---
+++ 017-SprintIntegrationTransformerExample/src/org/abhi/beans/Customer.java
@@ -0,0 +1,31 @@
+package org.abhi.beans;
+
+public class Customer {
+	private String name;
+	private String lastName;
+	private String city;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	@Override
+	public String toString() {
+		return "Customer [name=" + name + ", lastName=" + lastName + ", city="
+				+ city + "]";
+	}
+}
