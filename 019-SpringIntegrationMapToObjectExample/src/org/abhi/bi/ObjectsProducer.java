0a92a11da40bcc040a7a4b1ecfb8b8000f9dd696
abpandit <abpandit@DIN56001205.corp.capgemini.com>
2014-09-16 12:13:23 +0530

    Added new project spring integratioon map to object

---
+++ 019-SpringIntegrationMapToObjectExample/src/org/abhi/bi/ObjectsProducer.java
@@ -0,0 +1,39 @@
+package org.abhi.bi;
+
+import org.abhi.beans.Address;
+import org.abhi.beans.Customer;
+import org.springframework.context.support.GenericXmlApplicationContext;
+import org.springframework.integration.MessageChannel;
+import org.springframework.integration.core.PollableChannel;
+import org.springframework.integration.support.MessageBuilder;
+
+public class ObjectsProducer {
+	
+	public static void main(String[] args) {
+		GenericXmlApplicationContext ctx = new GenericXmlApplicationContext("beans-config.xml");
+		MessageChannel messageChannel = ctx.getBean("inputChannel", MessageChannel.class);
+		messageChannel.send(MessageBuilder.withPayload(new Customer("Abhijeet", "Pandit", new Address("Vishrantwadi", "DP road", "Pune", "India"))).build());
+		messageChannel.send(MessageBuilder.withPayload(new Customer("John", "Constantine",  new Address("Park street", "DP road", "Wakad", "India"))).build());
+		messageChannel.send(MessageBuilder.withPayload(new Customer("Tony", "Stark",  new Address("Model colony", "Ambedkar chawk", "Aundh", "India"))).build());
+	
+		//PollableChannel outputChannel = ctx.getBean("outputChannel", PollableChannel.class);
+		
+		PollableChannel newOutputChannel = ctx.getBean("newOutputChannel3", PollableChannel.class);
+		
+		while(true) {
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			System.out.println("--->" + newOutputChannel.receive().getPayload());
+		}
+		
+		
+		
+		
+	}
+	
+}
